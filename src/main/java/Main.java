14a18fb502c0b6bc0759b9de829eb7b6d6202269
doriangoobie <344033485@tdsb.ca>
2026-05-07 03:53:16 +0000

    submit assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         GameBoard board = new GameBoard();
-        GamePiece dog = new GamePiece("Dog", "Green", 849);
-        GamePiece cat = new GamePiece("Cat", "Blue", 65);
-        board.placePiece(dog, 1, 1);
-        board.placePiece(cat, 2, 1);
+        GamePiece pawn = new GamePiece("Dog", "Green", 849);
+        GamePiece pawn1 = new GamePiece("Cat", "Blue", 65);
+        board.placePiece(pawn, 1, 1);
+        board.placePiece(pawn1, 2, 1);
         System.out.println(board);
     }
 }
